1fe61df7be2ec5cfa47c112f96da9d5cf74f597d
afrano <afranoamran16@gmail.com>
2017-05-22 23:01:41 +0700

    Java dan Php

--- DistanceBetween.php
+++ DistanceBetween.php
@@ -2,7 +2,7 @@
 
 echo 'Jarak Univ Sadhar ke Tugu Jogja : ' . getDistanceBetween(-7.778056, 110.3857267, -7.782865, 110.367084, 'K'). ' Kilometers';
 
-function getDistanceBetween($latitude1, $longitude1, $latitude2, $longitude2, $unit = 'Mi') {
+function getDistanceBetween($latitude1, $longitude1, $latitude2, $longitude2, $unit) {
     $theta = $longitude1 - $longitude2;
     $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
     $distance = acos($distance); 
